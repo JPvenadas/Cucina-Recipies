b33a7bb67c5cf10cff1a64dbaf833a35fe150b29
Jp Venadas <jaypheevenadas18@gmail.com>
2022-09-29 19:43:28 +0800

    remove duplicate recommendations

--- src/Components/Mini-components/searchRecipe Components/Recommendation.jsx
+++ src/Components/Mini-components/searchRecipe Components/Recommendation.jsx
@@ -10,7 +10,15 @@ const Recommendation = () => {
   const getRec = async()=>{
     const res = await fetch("https://random-data-api.com/api/food/random_food?size=21")
     const rec = await res.json()
-    SetRecArray(rec)
+    let dishArr = []
+    await rec.map(recipe =>{
+      dishArr.push(recipe.dish)
+    })
+    dishArr =  dishArr.filter((item, index) =>{
+      return dishArr.indexOf(item) === index;
+    })
+    SetRecArray(dishArr)
+    
   }
     useEffect(()=>{
       getRec()
@@ -18,7 +26,7 @@ const Recommendation = () => {
   return (
     <div className='flex flex-flow-1 justify-center'>
      {recArray? recArray.map(recipe =>(
-        <RecItems key={recipe.id} text={recipe.dish}/>
+        <RecItems key={recipe} text={recipe}/>
      )): ""}
     </div>
   )
